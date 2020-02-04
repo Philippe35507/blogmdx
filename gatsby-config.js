386f3adf20c61d5b99851a74ca400311349d45be
Philippe35507 <55062877+Philippe35507@users.noreply.github.com>
2020-02-04 23:50:23 +0000

    debut

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
-module.exports = {
-  /* Your site config here */
+module.exports= { 
+  siteMetadata: {
+    title: `Code Tahiche`,
+    description: `Blog Tutoriels Web Developement`
+  },
+  plugins: [
+  {
+    resolve: 'gatsby-plugin-mdx',
+    options:{
+      extensions:[`.mdx`, `.md`]}},
+  
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      path: `${__dirname}/posts`,
+      name: `posts`
+    }
+  }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { useSiteMetadata } from "../hooks/useSiteMetadata"
+import { Layout } from "../components/Layout"
 
-export default () => <div>Hello world!</div>
+export default ({data}) => {
+  
+  return (
+    <>
+        <Layout>
+            {data.allMdx.nodes.map(({excerpt, frontmatter}) =>
+                <>
+                    <h1>{frontmatter.title}</h1>
+                    <p>{frontmatter.date}</p>
+                    <p>{excerpt}</p>
+                </>
+            )}
+      </Layout>
+    </>
+  )
+}
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 100)
+        frontmatter {
+          title
+          date
+        }
+      }
+    }
+  }
+`
